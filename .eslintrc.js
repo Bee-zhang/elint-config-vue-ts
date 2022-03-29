1fe424ddba44c08a1440d2dcae0aef914320e80f
Bee <18482155765@163.com>
2022-03-29 16:26:04 +0800

    测试同步github和gitee

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,7 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    // eslint-config-prettier 的缩写
-    'prettier',
+    'prettier', // eslint-config-prettier 的缩写
   ],
   parserOptions: {
     ecmaVersion: 12,
